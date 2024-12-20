e0a49b1368c0084fe021134a21068966c6125abb
Valahdyo Arbandy <149990771+valahdyo404@users.noreply.github.com>
2024-12-20 13:40:24 +0700

    Time: 2 ms (99.85%), Space: 44.6 MB (80.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,28 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-       int[] map = new int[128];
-       int count = t.length();
-       int start = 0, end = 0, startIndex = 0, minLength = Integer.MAX_VALUE;
-            
-       for (char c: t.toCharArray()){
-            map[c]++;
-       }
-       char[] charS = s.toCharArray();
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, minLength = Integer.MAX_VALUE, startIndex = 0;
 
-       while (end < charS.length) {
-            if (map[charS[end++]]-- > 0) {
+        for (char c: t.toCharArray()) {
+            map[c]++;
+        }
+
+        char[] charS = s.toCharArray();
+        for (int end = 0; end < charS.length; end++) {
+            if (map[charS[end]] > 0) {
                 count--;
             }
+            map[charS[end]]--;
             while (count == 0) {
                 if (end - start < minLength) {
                     startIndex = start;
                     minLength = end - start;
                 }
-                if (map[charS[start++]]++ == 0) {
+
+                if (map[charS[start]] == 0) {
                     count++;
                 }
+                map[charS[start]]++;
+                start++;
             }
         }
-       return minLength == Integer.MAX_VALUE ? new String() : new String(charS, startIndex, minLength);
+        return minLength == Integer.MAX_VALUE ? new String() : new String(charS, startIndex, minLength+1); 
     }
 }
