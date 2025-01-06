97fefb3be481e2562ced1e21da735c3bde271b88
Valahdyo Arbandy <149990771+valahdyo404@users.noreply.github.com>
2025-01-06 11:16:45 +0700

    Time: 0 ms (100%), Space: 40.4 MB (44.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int curr = 0;
+        for (int i = 4; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+}
