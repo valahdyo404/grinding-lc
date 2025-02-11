e3ba7daa270b2dacda61b1c6ee872ee0f00c58eb
Valahdyo Arbandy <149990771+valahdyo404@users.noreply.github.com>
2025-02-11 14:57:03 +0700

    Time: 25 ms (71.3%), Space: 40.5 MB (49.41%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,15 +3,12 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l = 1, r = n;
+        int l = 0, r = n;
 
         while (l < r) {
             int m = l + ((r - l) / 2);
-            if (!isBadVersion(m)) {
-                l = m + 1;
-            } else {
-                r = m;
-            }
+            if (!isBadVersion(m)) l = m + 1;
+            else r = m;
         }
         return l;
     }
